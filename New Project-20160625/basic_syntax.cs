74a1af02cd8d74d8c2b0a4264c0a54631f8586a2
khiem291 <khiem291@yahoo.com>
2016-06-26 23:29:35 +0700

    Update basic_syntax.cs
    casting to integer

--- New Project-20160625/basic_syntax.cs
+++ New Project-20160625/basic_syntax.cs
@@ -7,10 +7,10 @@ class Multible{
     // member variable
     double len = 2;
     double width = 5;
-    dynamic e = 4;
+    // dynamic e = 4;
     
     public void InputMul(){//(double len, double witdth){
-        Console.WriteLine("Input {0}", e);
+        Console.WriteLine("Input {0}, {1}", len, width);
     }
     public void ResultMul(){
         Console.WriteLine("Result: {0}",len * width);
@@ -21,6 +21,11 @@ class ExecuteMul{
         Multible Mul = new Multible();
         Mul.InputMul();
         Mul.ResultMul();
+        
+        // Input from console
+        int num;
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input value: {0}", num);
         Console.ReadLine();
     }
 }
